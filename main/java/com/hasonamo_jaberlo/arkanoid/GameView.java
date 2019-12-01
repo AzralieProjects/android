e4be38d8bd5425e7a4ac3421e95954632fd41b1a
luffyUnleashed <37274365+luffyUnleashed@users.noreply.github.com>
2019-12-01 20:54:06 +0200

    Add files via upload
    just added tho border limit

--- main/java/com/hasonamo_jaberlo/arkanoid/GameView.java
+++ main/java/com/hasonamo_jaberlo/arkanoid/GameView.java
@@ -26,11 +26,11 @@ public class GameView extends View {
     int Brick_Col = 4;
     private int startTop;
     int psteps, pleft, pright;
-    int ball_speed =3;
-   private int limit_arr[];
+    int ball_speed =15;
+    private int limit_arr[];
     Boolean startgame = false;
     int row_limet [];
-  int first_move=0;
+    int first_move=0;
     public GameView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         ball = new Ball();
@@ -71,9 +71,12 @@ public class GameView extends View {
         draw_paddle(canvas);
         draw_ball(canvas);
         if(startgame==true){
-                                move_ball();
-                                postInvalidate();
-                                first_move=1;
+            move_ball();
+       postInvalidate();
+                    check_boundres();
+
+
+            first_move=1;
 
         }
 ///////////////////////this will update the circle
@@ -87,23 +90,24 @@ public class GameView extends View {
 //            }
 //        }).start();
 
+//
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                while (startgame == true) {
+//                    check_boundres();
+//                }
+//            }
+//        }).start();
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (startgame == true) {
-                    check_boundres();
-                }
-            }
-        }).start();
     }
-private void check_boundres(){
-        if(ball.getXball()-ball.getRadios()==0||ball.getYball()-ball.getRadios()==0){
+    private void check_boundres(){
+        if(ball.getXball()-ball.getRadios()<=0||ball.getYball()-ball.getRadios()<=0||ball.getXball()+ball.getRadios()>=this.canvasW||ball.getYball()+ball.getRadios()>=this.canvasH ){
             ball_speed=ball_speed*-1;
         }
 
 
-}
+    }
     private void move_paddle() {
 
     }
@@ -121,7 +125,7 @@ private void check_boundres(){
 
 
     private void draw_ball(Canvas canvas) {
-  canvas.drawCircle(ball.getXball(),ball.getYball(),ball.getRadios(),ball_pen);
+        canvas.drawCircle(ball.getXball(),ball.getYball(),ball.getRadios(),ball_pen);
 
     }
 
@@ -143,7 +147,7 @@ private void check_boundres(){
     }
 
     private void draw_paddle(Canvas canvas) {
-         pleft=  (int)(this.paddle.getXcPaddle());
+        pleft=  (int)(this.paddle.getXcPaddle());
         pright=pleft+psteps;
 //        xpaddle=onTouchEvent(MotionEvent event);
 
@@ -157,14 +161,14 @@ private void check_boundres(){
 
         int steps =canvasH/18;
         int tempbottom=temptop+steps;
-            int left=10;
-           int leftsteps=canvasW/(Brick_Col);
-           int right=left+leftsteps;
-           int pad=20;
+        int left=10;
+        int leftsteps=canvasW/(Brick_Col);
+        int right=left+leftsteps;
+        int pad=20;
 //           int line=0;
         for(int col=0;col<Brick_Col;col++) {
 
-              int line=0;
+            int line=0;
             for (int i = 0; i < this.limit_arr[line]; i++) {
 
                 canvas.drawRect(left, temptop, right, temptop + steps, brick_pen);
@@ -172,9 +176,9 @@ private void check_boundres(){
 
             }
             line++;
-               left=(right+5);
-               right=(left+leftsteps);
-               temptop=startTop;
+            left=(right+5);
+            right=(left+leftsteps);
+            temptop=startTop;
 //             tempbottom=temptop+steps;
 
         }
@@ -188,7 +192,7 @@ private void check_boundres(){
         canvasH = h;
         Log.d(getClass().getName(), String.format("value = %d", w));
         startTop=canvasH/8;
-         psteps=canvasW/4;
+        psteps=canvasW/4;
         Paddle_x=canvasW/3;
         this.paddle.setXcPaddle(Paddle_x);
 
